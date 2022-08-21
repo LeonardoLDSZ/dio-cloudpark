754cde331578493a9a1434a8824285b608448f98
LeonardoLDSZ <leonardodavidesouza@gmail.com>
2022-08-21 17:32:06 -0300

    first commit

---
+++ src/main/java/one/digitalinnovation/diocloudpark/DioCloudparkApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.diocloudpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioCloudparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioCloudparkApplication.class, args);
+	}
+
+}
